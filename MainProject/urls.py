3a987cb61fd9ce182ee792dcc2b151c96bd76711
BeholdenArt <priyanshub5645@gmail.com>
2022-07-11 12:44:57 +0530

    Removed whitenoise from settings and changed url to re_path

--- MainProject/urls.py
+++ MainProject/urls.py
@@ -13,19 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import re
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 import FeeStructure
 
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('FeeStructure.urls')), 
  
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
